d8227525ec171eba7ece0b1a40d1898cc58dc106
fengll <732038907@qq.com>
2019-10-08 16:14:29 +0800

    shopmalladd

--- sever/router/shop.js
+++ sever/router/shop.js
@@ -3,21 +3,103 @@ const router = express.Router();
 
 // 连接数据库
 const getMysql = require("../mysql")
-// home商品
+    // home商品
 router.get("/", (req, res) => {
     // 参数结构
-    let {classname_1,classname_2,classname_3} = req.query;
+    let { classname_1, classname_2, classname_3 } = req.query;
     // console.log(classname_1,classname_2,classname_3)
     // 获取数据库
     const db = getMysql();
     // 连接数据库
     db.connect();
     // 数据库指令，结构的参数，函数输出结果
-    db.query("SELECT * FROM shop WHERE classname_3=?",[classname_3], (err, sqlRes) => {
-        // 把结果返回前端
-        res.send(sqlRes)
-    })
-    // 断开数据库连接
+    db.query("SELECT * FROM shop WHERE classname_3=?", [classname_3], (err, sqlRes) => {
+            // 把结果返回前端
+            res.send(sqlRes)
+        })
+        // 断开数据库连接
+    db.end();
+})
+router.get("/Hotpart", (req, res) => {
+    // 参数结构
+    let { classname_3 } = req.query;
+    // 获取数据库
+    const db = getMysql();
+    // 连接数据库
+    db.connect();
+    // 数据库指令，结构的参数，函数输出结果
+    db.query("SELECT * FROM shop WHERE classname_3 = ?", [classname_3], (err, sqlRes) => {
+            // 把结果返回前端
+            console.log(classname_3);
+            res.send(sqlRes);
+        })
+        // 断开数据库连接
+    db.end();
+})
+router.get("/boutique", (req, res) => {
+    // 参数结构
+    let { classname_3 } = req.query;
+    // 获取数据库
+    const db = getMysql();
+    // 连接数据库
+    db.connect();
+    // 数据库指令，结构的参数，函数输出结果
+    db.query("SELECT * FROM shop WHERE classname_3 = ?", [classname_3], (err, sqlRes) => {
+            // 把结果返回前端
+            console.log(classname_3);
+
+            res.send(sqlRes);
+        })
+        // 断开数据库连接
+    db.end();
+})
+router.get("/mountings", (req, res) => {
+    // 参数结构
+    let { classname_3 } = req.query;
+    // 获取数据库
+    const db = getMysql();
+    // 连接数据库
+    db.connect();
+    // 数据库指令，结构的参数，函数输出结果
+    db.query("SELECT * FROM shop WHERE classname_3 = ?", [classname_3], (err, sqlRes) => {
+            // 把结果返回前端
+            console.log(classname_3);
+
+            res.send(sqlRes);
+        })
+        // 断开数据库连接
+    db.end();
+})
+router.get("/panic", (req, res) => {
+    // 参数结构
+
+    // 获取数据库
+    const db = getMysql();
+    // 连接数据库
+    db.connect();
+    // 数据库指令，结构的参数，函数输出结果
+    db.query("SELECT * FROM panic", (err, sqlRes) => {
+            // 把结果返回前端
+            res.send(sqlRes)
+        })
+        // 断开数据库连接
+    db.end();
+})
+router.get("/category", (req, res) => {
+    // 参数结构
+
+    // 获取数据库
+    const db = getMysql();
+    // 连接数据库
+    db.connect();
+    // 数据库指令，结构的参数，函数输出结果
+    db.query("SELECT * FROM shop", (err, sqlRes) => {
+            // 把结果返回前端
+
+
+            res.send(sqlRes);
+        })
+        // 断开数据库连接
     db.end();
 })
 
