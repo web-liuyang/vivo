d460abc3914e0a0dee470b56b8a478621587ec73
1997liuyuan <1275527696@qq.com>
2019-10-08 09:02:22 +0800

    首页所有，详情页，购物车

--- client/src/js/public.js
+++ client/src/js/public.js
@@ -1,1 +1,47 @@
 //公共
+$(function() {
+    /* 首页 */
+
+    $(document).scroll(function() {
+        let offset = $(document).scrollTop();
+        if (offset > 100) {
+            $(".app-bar").addClass("flexd");
+            $(".respond-nav").addClass("flexd")
+        }
+        if (offset < 100) {
+            $(".app-bar").removeClass("flexd")
+        }
+
+    })
+
+
+    // 折叠栏
+    let $boxItem = $(".box-item");
+    $boxItem.hover(function() {
+            $(".list-box").slideDown(500);
+        }, function() {
+            $(".list-box").slideUp(500);
+        })
+        // 导航栏折叠后的折叠按钮
+    $(".unfold-btn").click(function() {
+        $("header").toggleClass("none");
+        $("main").toggleClass("none");
+        $("footer").toggleClass("none");
+
+        let $logobar = $(".logobar")
+        if ($logobar.hasClass("none")) {
+            $logobar.removeClass("none");
+            $(".search-bar").removeClass("block");
+        } else {
+            $logobar.addClass("none");
+            $(".search-bar").addClass("block");
+            $("body,html").css({
+                "background": "#ffffff"
+            })
+        }
+
+
+    })
+
+
+})
