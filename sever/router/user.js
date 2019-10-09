5bc9d8cd27223b99f47604e17fd8e305fde3499e
web-liuyang <984584014@qq.com>
2019-10-09 20:05:16 +0800

    保存到本地

--- sever/router/user.js
+++ sever/router/user.js
@@ -3,27 +3,27 @@ const router = express.Router();
 
 // 连接数据库
 const getMysql = require("../mysql")
-    //登陆
+//登陆
 router.post("/login", (req, res) => {
     // 参数结构
-    let { username, password } = req.body;
+    let {username,password}=req.body;
     let mysqlyuju = "SELECT * FROM user WHERE `username`=?";
     let jiegou = [username]
-        // 获取数据库
+    // 获取数据库
     const db = getMysql();
     // 连接数据库
     db.connect();
     // 数据库指令，结构的参数，函数输出结果
-    db.query(mysqlyuju, jiegou, (err, sqlRes) => {
-            if (sqlRes.length == 0) {
-                res.send({ tishi: "账号不存在" })
-            } else if (sqlRes[0].password == password) {
-                res.send({ tishi: "登陆成功" })
-            } else {
-                res.send({ tishi: "密码错误" })
-            }
-        })
-        // 断开数据库连接
+    db.query(mysqlyuju,jiegou, (err, sqlRes) => {
+        if(sqlRes.length==0){
+            res.send({tishi:"账号不存在"})
+        }else if(sqlRes[0].password==password){
+            res.send({tishi:"登陆成功",data:sqlRes})
+        }else{
+            res.send({tishi:"密码错误"})
+        }
+    })
+    // 断开数据库连接
     db.end();
 })
 
@@ -33,22 +33,22 @@ router.post("/login", (req, res) => {
 // 注册
 router.post("/regist", (req, res) => {
     // 参数结构
-    let { username, password, email, tel, nikename } = req.body;
+    let {username,password,email,tel,nike}=req.body;
     let mysqlyuju = "INSERT INTO user (`username`,`password`,`email`,`tel`,`nikename`) VALUES (?,?,?,?,?)";
-    let jiegou = [username, password, email, tel, nikename]
-        // 获取数据库
+    let jiegou = [username,password,email,tel,nike]
+    // 获取数据库
     const db = getMysql();
     // 连接数据库
     db.connect();
     // 数据库指令，结构的参数，函数输出结果
-    db.query(mysqlyuju, jiegou, (err, sqlRes) => {
-            if (err) {
-                res.send({ tishi: "账号已存在" })
-            } else {
-                res.send({ tishi: "注册成功" })
-            }
-        })
-        // 断开数据库连接
+    db.query(mysqlyuju,jiegou, (err, sqlRes) => {
+        if(err){
+            res.send({tishi:"账号已存在"})
+        }else{
+            res.send({tishi:"注册成功"})
+        }
+    })
+    // 断开数据库连接
     db.end();
 })
 
