ffdc9edb16cf5974998b1f768f92b3cb48e2cc51
web-liuyang <984584014@qq.com>
2019-10-09 18:56:16 +0800

    主页

---
+++ client/src/js/public.js
@@ -0,0 +1,79 @@
+//公共
+$(function () {
+
+
+    // 折叠栏
+
+    //二级菜单
+/*     $erji = $(".nav-item.box-item");
+    $erjisun = $(".nav-item.box-item ul");
+    $erji.mouseenter(function () { 
+        index = $(this).index();
+        // console.log($(this).index())
+        $erjisun[index].classList.add("block")
+    });
+    $erji.mouseleave(function () { 
+        index = $(this).index();
+        // console.log($(this).index())
+        $erjisun[index].classList.remove("block")
+    });
+ */
+
+
+
+
+
+    // 导航栏折叠后的折叠按钮
+    $(".unfold-btn").click(function () {
+        $("header").toggleClass("none");
+        $("main").toggleClass("none");
+        $("footer").toggleClass("none");
+
+        let $logobar = $(".logobar")
+        if ($logobar.hasClass("none")) {
+            $logobar.removeClass("none");
+            $(".search-bar").removeClass("block");
+        } else {
+            $logobar.addClass("none");
+            $(".search-bar").addClass("block");
+            $("body,html").css({
+                "background": "#ffffff"
+            })
+        }
+
+
+    })
+
+
+    $(".box-item").hover(function(){
+      
+        $(".list-box").css({
+            "display":"block"
+        });
+        $(".box-item span").css({
+            "color":"#000000"
+        });
+        $(".list-ll a").css({
+            "color":"#000000"
+        });
+        $(".navbar-brand").css({
+            "color":"#000000"
+        })
+     
+    },function(){
+        $(".list-box").css({
+            "display":"none"
+        });
+        $(".box-item span").css({
+            "color":"white"
+        });
+        $(".list-ll a").css({
+            "color":"white"
+        });
+        $(".navbar-brand").css({
+            "color":"white"
+        });
+    })
+
+
+})
