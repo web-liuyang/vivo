6716879f1930723052ff693faba948da8f3510bb
1997liuyuan <1275527696@qq.com>
2019-10-10 09:05:05 +0800

    登录注册判断

--- client/src/js/public.js
+++ client/src/js/public.js
@@ -1,30 +1,32 @@
+import { Z_BLOCK } from "zlib";
+
 //公共
-$(function () {
+$(function() {
 
 
     // 折叠栏
 
     //二级菜单
-/*     $erji = $(".nav-item.box-item");
-    $erjisun = $(".nav-item.box-item ul");
-    $erji.mouseenter(function () { 
-        index = $(this).index();
-        // console.log($(this).index())
-        $erjisun[index].classList.add("block")
-    });
-    $erji.mouseleave(function () { 
-        index = $(this).index();
-        // console.log($(this).index())
-        $erjisun[index].classList.remove("block")
-    });
- */
+    /*     $erji = $(".nav-item.box-item");
+        $erjisun = $(".nav-item.box-item ul");
+        $erji.mouseenter(function () { 
+            index = $(this).index();
+            // console.log($(this).index())
+            $erjisun[index].classList.add("block")
+        });
+        $erji.mouseleave(function () { 
+            index = $(this).index();
+            // console.log($(this).index())
+            $erjisun[index].classList.remove("block")
+        });
+     */
 
 
 
 
 
     // 导航栏折叠后的折叠按钮
-    $(".unfold-btn").click(function () {
+    $(".unfold-btn").click(function() {
         $("header").toggleClass("none");
         $("main").toggleClass("none");
         $("footer").toggleClass("none");
@@ -45,35 +47,61 @@ $(function () {
     })
 
 
-    $(".box-item").hover(function(){
-      
+    $(".box-item").hover(function() {
+
         $(".list-box").css({
-            "display":"block"
+            "display": "block"
         });
         $(".box-item span").css({
-            "color":"#000000"
+            "color": "#000000"
         });
         $(".list-ll a").css({
-            "color":"#000000"
+            "color": "#000000"
         });
         $(".navbar-brand").css({
-            "color":"#000000"
+            "color": "#000000"
         })
-     
-    },function(){
+
+    }, function() {
         $(".list-box").css({
-            "display":"none"
+            "display": "none"
         });
         $(".box-item span").css({
-            "color":"white"
+            "color": "white"
         });
         $(".list-ll a").css({
-            "color":"white"
+            "color": "white"
         });
         $(".navbar-brand").css({
-            "color":"white"
+            "color": "white"
         });
     })
+
+
+
+
+    // 判断登录状态
+    if (!sessionStorage.user) {
+        $(".dl-login").css({
+            "display": "block"
+        });
+        $(".zc-regist").css({
+            "display": "block"
+        });
+        $(".personal-list").css({
+            "display": "none"
+        })
+    } else {
+        $(".dl-login").css({
+            "display": "none"
+        });
+        $(".zc-regist").css({
+            "display": "none"
+        });
+        $(".personal-list").css({
+            "display": "block"
+        })
+    }
 
 
 })
