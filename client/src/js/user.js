5710d16ef2fcff5e1dbe8cd8ae0f5001b3b3c265
web-liuyang <984584014@qq.com>
2019-10-11 16:13:03 +0800

    user.js错误更改

--- client/src/js/user.js
+++ client/src/js/user.js
@@ -4,7 +4,7 @@ import "../sass/user.scss";
 //引用JS
 import "../js/fn.js";
 import "../js/loadding.js";
-import "../js/public.js";
+// import "../js/public.js";
 //引用bootstrap
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.css';
